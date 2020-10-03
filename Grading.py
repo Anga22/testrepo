87d5a73a4492869452562fbaa098d9081c56974f
Anga22 <68614505+Anga22@users.noreply.github.com>
2020-10-03 18:33:03 +0200

    Add files via upload

---
+++ Grading.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[58]:
+
+
+x = int(input())
+if x < 50 :
+    y = ('Fail')
+if x >= 75 :
+      y = ('First')
+if 70 <= x <= 74 :
+     y = ('Upper second')
+if 60 <= x <= 69  :
+   y = ('Lower second')
+if 50 <= x <= 59 :
+    y = ('Third')
+print(y)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
